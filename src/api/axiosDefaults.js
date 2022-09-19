337296b854be9ae13761ce24fae189fb68c30aa5
kkwong44 <wong.k@sky.com>
2022-09-19 21:48:00 +0000

    Add access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -5,3 +5,7 @@ import axios from "axios";
 axios.defaults.baseURL = "https://kkimages-drf-api.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// axios interceptors request and response
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
