62395c1d37ec963538b6f1380fec0c8822b6d3ac
kkwong44 <wong.k@sky.com>
2022-09-19 14:02:52 +0000

    Install and set  react routing to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,7 @@
 // 
 import React from "react";
 import { Navbar, Container, Nav} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import logo from "../assets/kki-logo.png"
 import styles from "../styles/NavBar.module.css"
 
@@ -10,15 +11,36 @@ const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
             <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link><i className="fas fa-home"></i>Home</Nav.Link>
-            <Nav.Link><i className="fas fa-sign-in-alt"></i>Sign in</Nav.Link>
-            <Nav.Link><i className="fas fa-user-plus"></i>Sign up</Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signup"
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
