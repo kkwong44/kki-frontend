54fd502f4a10287859e443f0fd4f0245c8915e9a
kkwong44 <wong.k@sky.com>
2022-10-06 00:30:49 +0000

    Update the contents in About page

--- src/pages/abouts/AboutPage.js
+++ src/pages/abouts/AboutPage.js
@@ -19,10 +19,14 @@ function AboutPage() {
             <Card.Text>
               KK Images is a free online service for photographers who want to
               showcase their works. There are no restrictions on the
-              photographer’s skills to register and post their works. Non
+              photographer's skills to register and post their works. Non
               photographers can also register to the site and follow the
               photographers. They can like and leave comments to the works
               posted by the photographers.
+            </Card.Text>
+            <Card.Text>
+              The registered photographers on this sites can be contacted when
+              they leave an email address in their profile page.
             </Card.Text>
           </Card.Body>
           <Card.Img variant="bottom" src={hero} alt="hero image" />
