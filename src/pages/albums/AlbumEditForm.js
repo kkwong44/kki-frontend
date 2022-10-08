e27649eeb1d833a5c32f277b22a9a934e0cddf10
kkwong44 <wong.k@sky.com>
2022-10-08 15:42:54 +0000

    Add new skill level field to edit form

--- src/pages/albums/AlbumEditForm.js
+++ src/pages/albums/AlbumEditForm.js
@@ -27,13 +27,14 @@ const AlbumEditForm = () => {
     content: "",
     category_filter: "",
     cover_image: "",
+    skill_level: "",
   });
 
   const [categoryList, setCategoryList] = useState([
     { display_name: "", value: "" },
   ]);
 
-  const { title, content, category_filter, cover_image } = albumData;
+  const { title, content, category_filter, cover_image, skill_level } = albumData;
   const imageInput = useRef(null);
   const history = useHistory();
   const { id } = useParams();
@@ -43,10 +44,10 @@ const AlbumEditForm = () => {
     const handleMount = async () => {
       try {
         const { data } = await axiosReq.get(`/albums/${id}`);
-        const { title, content, category_filter, cover_image, is_owner } = data;
+        const { title, content, category_filter, cover_image, skill_level, is_owner } = data;
 
         is_owner
-          ? setAlbumData({ title, content, category_filter, cover_image })
+          ? setAlbumData({ title, content, category_filter, cover_image, skill_level })
           : history.push("/");
       } catch (err) {
         // console.log(err)
@@ -72,6 +73,16 @@ const AlbumEditForm = () => {
     });
   };
 
+  // Handle radio selection
+  const handleRadioChange = event => {
+    setAlbumData(event.target.value);
+    setAlbumData({
+      ...albumData,
+      skill_level: event.target.value,
+    });
+    console.log(skill_level);
+  };
+
   // Handle change in image upload
   const handleChangeImage = (event) => {
     if (event.target.files.length) {
@@ -91,6 +102,7 @@ const AlbumEditForm = () => {
     formData.append("title", title);
     formData.append("content", content);
     formData.append("category_filter", category_filter);
+    formData.append("skill_level", skill_level);
     if (imageInput?.current?.files[0]) {
       formData.append("cover_image", imageInput.current.files[0]);
     }
@@ -116,6 +128,7 @@ const AlbumEditForm = () => {
           name="title"
           value={title}
           onChange={handleChange}
+          aria-label="title"
         />
       </Form.Group>
       {errors.title?.map((message, idx) => (
@@ -132,6 +145,7 @@ const AlbumEditForm = () => {
           value={category_filter}
           placeholder="General"
           onChange={handleChange}
+          aria-label="category"
         >
           {categoryList.map((category) => (
             <option value={category.value} key={category.value}>
@@ -145,6 +159,60 @@ const AlbumEditForm = () => {
           {message}
         </Alert>
       ))}
+      {/* Album skill level radio button input */}
+      <Form.Group>
+        <Form.Label>Skill Level</Form.Label>
+        <Container className={styles.Content}>
+          <div key="inline-radio" className="mb-3">
+            <Form.Check
+              className="mr-0"
+              type="radio"
+              id="inline-radio-1"
+              inline
+              value="Professional"
+              checked={skill_level === "Professional"}
+              onClick={handleRadioChange}
+              aria-label="professional"
+            />
+            <Form.Label className="pr-4">Professional</Form.Label>
+            <Form.Check
+              className="mr-0"
+              type="radio"
+              id="inline-radio-2"
+              inline
+              value="Amateur"
+              checked={skill_level === "Amateur"}
+              onClick={handleRadioChange}
+              aria-label="amateur"
+            />
+            <Form.Label>Amateur</Form.Label>
+            <div>
+            </div>
+            <Form.Check
+              className="mr-0"
+              type="radio"
+              id="inline-radio-2"
+              inline
+              value="Hobbyist"
+              checked={skill_level === "Hobbyist"}
+              onClick={handleRadioChange}
+              aria-label="hobbyist"
+            />
+            <span className="pr-4" >Hobbyist</span>
+            <Form.Check
+              className="mr-0"
+              type="radio"
+              id="inline-radio-3"
+              inline
+              value="Other"
+              checked={skill_level === "Other"}
+              onClick={handleRadioChange}
+              aria-label="other"
+            />
+            <span className="pr-4" >Other</span>
+          </div>
+        </Container>
+      </Form.Group>
       {/* Album descripton input */}
       <Form.Group>
         <Form.Label>Content</Form.Label>
@@ -154,6 +222,7 @@ const AlbumEditForm = () => {
           name="content"
           value={content}
           onChange={handleChange}
+          aria-label="content"
         />
       </Form.Group>
       {errors.content?.map((message, idx) => (
@@ -185,7 +254,7 @@ const AlbumEditForm = () => {
             {/* Upload cover image area */}
             <Form.Group className="text-center">
               <figure>
-                <Image className={appStyles.Image} src={cover_image} rounded />
+                <Image className={appStyles.Image} src={cover_image} alt={title} rounded />
               </figure>
               <div>
                 <Form.Label
